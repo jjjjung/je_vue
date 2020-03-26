ef9ce44f6c35cfa8fe4ed3b365bf4e47912bbf38
jjjjung <62410127+jjjjung@users.noreply.github.com>
2020-03-26 15:49:31 +0900

    0

--- je/src/App.vue
+++ je/src/App.vue
@@ -1,21 +1,20 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
     <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
-      <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
-      <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
-    </ul>
+    <section>
+      <b-card no-body>
+        <b-tabs pills card vertical>
+          <b-tab title="git" active>
+            <b-card-text>
+              <h5>git clone</h5>
+              <p>서버의 모든 파일을 로컬로 다운로드</p>
+            </b-card-text>
+          </b-tab>
+          <b-tab title="Tab 2"><b-card-text>Tab contents 2</b-card-text></b-tab>
+          <b-tab title="Tab 3"><b-card-text>Tab contents 3</b-card-text></b-tab>
+        </b-tabs>
+      </b-card>
+    </section>
   </div>
 </template>
 
@@ -24,37 +23,12 @@ export default {
   name: 'app',
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      msg: 'JE vue'
     }
   }
 }
 </script>
 
 <style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 
-h1, h2 {
-  font-weight: normal;
-}
-
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-
-a {
-  color: #42b983;
-}
 </style>
